d98ece9fcfa0a8d9e1c3f4122c6301652328902b
melcd238 <melcd238@gmail.com>
2020-10-23 16:30:46 +0200

    mise en page inde

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-// je stocke l'URL de l'API dans une variable
-const url = "http://localhost:3000/api/teddies";
-let teddies="";
+
+const url = "http://localhost:3000/api/teddies";// je stocke l'URL de l'API dans une variable
+let teddies=""; 
 const teddiesContainer = document.querySelector('#teddies-container');
 
 // je crée ma requete
@@ -15,40 +15,31 @@ requete.onload = function () {
              teddies = requete.response; // je stocke la reponse dans une variable.
             console.log(teddies);
             afficherTeddies(); // J'affiche dynamiquement les teddies en utilisant la fonction.
-           
-
-        }
+            }
         
     } else {
         alert("Un problème est survenu, merci de réessayer plus tard");
     }
  }
-// creation de la fonction pour afficher un teddi
- function afficherTeddie(item) {
-     const itemElement = document.createElement('div');  
-     itemElement.innerHTML = ` <div class="col-lg-4 col-md-6 mb-4 item-card"> 
-     <div class="card text-center">
-       <div class="card-header">
-         <h2> ${item.name}</h2>
-         <p> ${item.price} € </p>
-       </div>
-       <div class="card-body">
-         <img class="card-img-top" src="${item.imageUrl}" alt="">
-         <p class="card-text">${item.description} </p>
-         
-       </div>
-       <div class="card-footer text-muted">
-         <button type="button" class="btn btn-secondary btn-lg btn-block" id="${item._id}"><a href="product.html"> Sélectionnez moi </a> </button>
-       </div>
-       </div>
-     </div>`;
-    teddiesContainer.appendChild(itemElement);
-     
- }
- // creation de la fonction pour afficher les teddies. 
+  // creation de la fonction pour afficher les teddies. 
   function afficherTeddies() {
     teddies.forEach(afficherTeddie);
 }
+// creation de la fonction pour afficher un teddi
+ function afficherTeddie(item) {
+     const itemElement = document.createElement('div');  
+     itemElement.setAttribute("class","col-lg-5 col-md-6 mb-4 item-card")
+     itemElement.innerHTML = `  <div class="card text-center"> <div class="card-header"><h2> ${item.name}</h2>
+         <p> ${item.price/100} € </p> </div>
+        <div class="card-body"><img class="card-img-top" src="${item.imageUrl}" alt="">
+         <p class="card-text">${item.description} </p> </div>
+         <div class="card-footer text-muted">
+         <button type="button" class="btn btn-secondary btn-lg btn-block" id="${item._id}"><a href="product.html"> Sélectionnez moi </a> </button>
+       </div> </div>`;
+    teddiesContainer.appendChild(itemElement);
+     
+ }
+
 
 
 
