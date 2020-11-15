6723e0dafc636a8793ba195b8ea62d399224d18b
melcd238 <melcd238@gmail.com>
2020-11-15 22:27:59 +0100

    recup de la confirmation dans localStorage

--- confirm.js
+++ confirm.js
@@ -1,1 +1,5 @@
 const confirmOrder = document.getElementById('confirmationOrder');
+// recuperation de la reponse de confirmation
+let confirmation = JSON.parse(localStorage.getItem("confirm"));
+console.log(confirmation);
+// affichage de la confirmation de commande
