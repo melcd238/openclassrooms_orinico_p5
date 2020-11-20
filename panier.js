4356d4b33b0d821c8515970e3e7484300c56343b
melcd238 <melcd238@gmail.com>
2020-11-20 16:11:37 +0100

    resolution bug deleteTeddi

--- panier.js
+++ panier.js
@@ -1,8 +1,7 @@
-const teddiContainerPanier = document.querySelector("#container-panier");
+const divAffichageTeddiStore = document.querySelector(".affichageTeddiStore");
 const validation = document.querySelector('#validate');
+const teddiContainerPanier = document.querySelector("#container-panier2");
 const containerForm = document.querySelector("#container-form");
-let teddiesStore = recupTeddies();
-
 
 // recuperer les données stocker dans le localstorage 
 function recupTeddies() {
@@ -13,10 +12,11 @@ function recupTeddies() {
     return teddiesStore;  
 }
 
+let teddiesStore = recupTeddies();
+console.log(teddiesStore);
 
-
-// affichage des articles dans le panier 
-// si le panier est vide
+// fonction pour afficher les teddi
+// si le panier est vide :
 if (teddiesStore.length === 0 || teddiesStore === null && getComputedStyle(validation).display == "block") {
     validation.style.display =" none";
     let panierVide = document.createElement("div");
@@ -25,115 +25,93 @@ if (teddiesStore.length === 0 || teddiesStore === null && getComputedStyle(valid
     teddiContainerPanier.appendChild(panierVide);
     
     
-
+// s'il y a des teddi dans le panier : 
 } else {
-    // si il y a des produits dans le panier
-    // on affiche les articles dans le panier
-    for (let teddiInStore of teddiesStore) {
-        console.log(teddiInStore);
-        let panierPlein = document.createElement("table");
-panierPlein.setAttribute("class","table table-striped")
-panierPlein.innerHTML = ` 
-  <thead>
-    <tr>
-      <th scope="col">Article</th>
-      <th scope="col">Prix unitaire</th>
-      <th scope="col">Quantité</th>
-      <th scope="col"> Prix total</th>
-      <th scope="col">Supprimer l'article</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr class="list-products-panier">
-      <td class="articlePanier">${teddiInStore.tedName} <img src="${teddiInStore.tedImage}" width= 80px heigt= 80px> ${teddiInStore.tedColor}</td>
-      <td class="prixPanier">${teddiInStore.tedPrice}€</td>
-      <td class="prixPanier">${teddiInStore.tedQuantite}</td>
-      <td class="prixPanier totalPrice"> ${teddiInStore.totalPrice}€  </td>
-      <td class="prixPanier trash"> <button class="deleteBtn" data-id=" ${teddiInStore.tedId}" data-color="${teddiInStore.tedColor}"><i class="far fa-trash-alt"></i></button></td>
-    </tr>
-  </tbody>
-</table>`
-teddiContainerPanier.appendChild(panierPlein);
-
-
- }
-    // on affiche le prix total de la commande
-    let arrayPrixTotal =[];
-    for (let teddiInStore of teddiesStore) {
-      let prix = teddiInStore.totalPrice;
-      arrayPrixTotal.push(prix);
-      console.log(arrayPrixTotal);
-      
-    }
-    let prixTotal = arrayPrixTotal.reduce((accumulater, valeurCourante)=> accumulater+ valeurCourante);
-    const prixTotalCommande = document.querySelector('#totalPricePanier');
-    prixTotalCommande.innerHTML= `PRIX TOTAL: ${prixTotal}€`
-    // on incrémente le panier
-     let arrayCompteurPanier =[] ;
-     for (const teddiInStore of teddiesStore) {
-       let itemQte = teddiInStore.tedQuantite;
-       arrayCompteurPanier.push(itemQte);
-     }
-     let compteurPanier = arrayCompteurPanier.reduce((accumulater, valeurCourante)=> accumulater+ valeurCourante);
-     let itemInCart = document.querySelector('#cart-qte');
-     itemInCart.innerHTML=`${compteurPanier}`
-
-    // on gère le button supprimer l'article
-    const deleteBtn = document.querySelectorAll(".deleteBtn");
-    console.log(deleteBtn);
-    for (let i = 0; i < deleteBtn.length; i++) {
-      console.log(deleteBtn[i].dataset.id);
-      console.log(deleteBtn[i].dataset.color);
-    }
-
-  
-    function deleteTeddi(e) {
-      e.preventDefault()
-      
-      if(window.confirm(`Voulez-vous supprimer cet article du panier?`)){
-        console.log(teddiesStore); // renvoie mon tableau d'objet 
-       
-       const index = teddiesStore.findIndex(index => index.TedId == this.dataset.id && index.color == this.dataset.color);
-        console.log(index);
-        teddiesStore.splice(index,1); 
-      
-        location.reload(alert(`Votre article a bien été supprimé`));
-
-        // enregistrement du nouveau localstorage
-      localStorage.setItem("teddiesInCart", JSON.stringify(teddiesStore)) 
-      JSON.parse(localStorage.getItem("teddiesInCart"));
-       
-      } 
-      else {
-        window.location.href = "panier.html";
-      }
+const displayTeddi = () => {
+    const teddiesStoreNode = teddiesStore.map((teddi , index) => {
+        return createTeddiElement(teddi, index);
+    });
+    divAffichageTeddiStore.innerHTML =" ";
+    // j'utilise l'operateur spread pour retourner une liste et pas un tableau
+    divAffichageTeddiStore.append(...teddiesStoreNode);
+};
+// fonction pour creer l'element  teddi 
+const createTeddiElement = (teddi , index) =>{
+     const ul = document.createElement('ul');
+     ul.setAttribute("class","ulDisposition");
+     ul.innerHTML = `
+     <li class="liDisp">Article
+     <ul>
+      <li>${teddi.tedName} </li>
+      <li><img src="${teddi.tedImage}" width= 80px heigt= 80px></li>
+      <li>${teddi.tedColor}</li>
+     </ul></li>
+      <li class="liDisp">Quantité<ul>
+      <li>${teddi.tedQuantite}</li></ul></li>
+      <li class="liDisp">Prix unitaire<ul>
+      <li>${teddi.tedPrice}</li></ul></li>
+     <li class="liDisp">Prix total<ul><li>${teddi.totalPrice}</li>
+     </ul></li>
+     <li class="liDisp">Supprimer
+     <ul>
+    <li> <button class="deleteBtn"><i class="far fa-trash-alt"></i></button></li>
+    </ul></li> `;
+    const btnDelete = ul.querySelector('.deleteBtn');
+    btnDelete.addEventListener('click', (event)=>{
+        deleteTeddi(index);
+    })
     
-      
+    return ul;
+};
+// fonction deleteTeddi qui sera appeler à l'interieur de l'evenement btnDelete
+const deleteTeddi = (index)=>{
+    teddiesStore.splice(index,1);
+    localStorage.setItem("teddiesInCart", JSON.stringify(teddiesStore)) 
+    JSON.parse(localStorage.getItem("teddiesInCart"));
+    displayTeddi();
+}
+// incrementation du panier:
+let arrayCompteurPanier =[] ;
+for (const teddiInStore of teddiesStore) {
+  let itemQte = teddiInStore.tedQuantite;
+  arrayCompteurPanier.push(itemQte); }
+let compteurPanier = arrayCompteurPanier.reduce((accumulater, valeurCourante)=> accumulater+ valeurCourante);
+let itemInCart = document.querySelector('#cart-qte');
+itemInCart.innerHTML=`${compteurPanier}`
+// calcul du prix total de la commande:
+let arrayPrixTotal =[];
+for (let teddiInStore of teddiesStore) {
+  let prix = teddiInStore.totalPrice;
+  arrayPrixTotal.push(prix);
+  console.log(arrayPrixTotal); }
+let prixTotal = arrayPrixTotal.reduce((accumulater, valeurCourante)=> accumulater+ valeurCourante);
+const prixTotalCommande = document.querySelector('#totalPricePanier');
+prixTotalCommande.innerHTML= `PRIX TOTAL: ${prixTotal}€`
+
+
+// fonction pour recupérer le formulaire au click sur le bouton validez votre commande:
+function form() {
+    if (getComputedStyle(containerForm).display == "none" ||getComputedStyle(validation).display == "block" || getComputedStyle(teddiContainerPanier).display == "block" ){
+      // on recupere la valeur courante de la propriété display sur les const avec getComputedStyle(const).display
+     containerForm.style.display = "block";
+     validation.style.display = "none";
+     teddiContainerPanier.style.display = "none";
     }
-    for(let i= 0;i < deleteBtn.length;i++){
-      deleteBtn[i].addEventListener('click', deleteTeddi)  
-            
-      };
-
-      // Gestion du bouton commander avec affichage du formulaire et disparition du bouton commander
-     function form() {
-       if (getComputedStyle(containerForm).display == "none" ||getComputedStyle(validation).display == "block" || getComputedStyle(teddiContainerPanier).display == "block" ){
-         // on recupere la valeur courante de la propriété display sur les const avec getComputedStyle(const).display
-        containerForm.style.display = "block";
-        validation.style.display = "none";
-        teddiContainerPanier.style.display = "none";
-       }
-     }
-     validation.addEventListener('click',form);
+  }
+validation.addEventListener('click',form);
 
 
-     // recupération des données du formulaire et de mon tableau de produit au click sur le bouton Envoyez votre commande : 
-     const submit = document.getElementById('submitorder');
-     submit.addEventListener('click',commandePanier);
+// recupération des données du formulaire et de mon tableau de produit au click sur le bouton Envoyez votre commande : 
+const submit = document.getElementById('submitorder');
+submit.addEventListener('click',(e)=>{
+     e.preventDefault();
+    commandePanier();
+});
 
-     function commandePanier(e) {
-       e.preventDefault();
-       let orderInput = document.getElementsByTagName('input');
+
+
+const commandePanier = () =>{
+     let orderInput = document.getElementsByTagName('input');
        if (orderInput[0].value && orderInput[1].value && orderInput[2].value && orderInput[3].value && orderInput[4].value ) {
         let contact = {
           firstName: orderInput[0].value,
@@ -154,7 +132,8 @@ teddiContainerPanier.appendChild(panierPlein);
        }
       let order = { contact, products };
       console.log(order);
-    
+
+
       // requete post 
       const reponseOrder =fetch("http://localhost:3000/api/teddies/order", {
         method: "POST",
@@ -186,22 +165,10 @@ teddiContainerPanier.appendChild(panierPlein);
          alert("Merci de remplir tous les champs!")
        } 
 
-       
-     }
     }
 
-    
 
 
-       
-  
- 
-        
-    
-   
-   
 
-    
-
-    
-
+displayTeddi();
+}
